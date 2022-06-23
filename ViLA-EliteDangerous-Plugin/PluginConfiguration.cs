b136f6382172f5e9d7d69efd76d8806c792b0449
Sven J <s.jantzen@me.com>
2022-06-23 17:25:31 +0200

    Projektdateien hinzufügen.

---
+++ ViLA-EliteDangerous-Plugin/PluginConfiguration.cs
@@ -0,0 +1,15 @@
+﻿// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable PropertyCanBeMadeInitOnly.Global
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace ViLA.Extensions.EliteDangerous;
+
+public class PluginConfiguration
+{
+    public int DelayMs { get; set; }
+
+    public static PluginConfiguration Default()
+    {
+        return new() { DelayMs = 500 };
+    }
+}
